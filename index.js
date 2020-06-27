39d3dae45a267e94614f834195e1561c18472865
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2020-06-26 20:35:19 -0400

    I edited the get20s function

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ function getArtistByIndex(id) {
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
 function get20s(array){
-  if (artists.years > 1899 && artists.years < 2001) {
+  if (artists.years > "1899" && artists.years < "2001") {
     return artists.name;
   }
 }
